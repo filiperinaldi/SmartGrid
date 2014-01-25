4ece23a00016df24da959d4d19da8a43417e823a
Filipe Rinaldi <filipe.rinaldi@gmail.com>
2014-01-25 17:57:54 +0000

    Initial content commit

--- README.md
+++ README.md
@@ -1,4 +1,135 @@
-SmartGrid
-=========
 
-SmartGrid is a JQuery plugin that presents data in different views (list or tiles) and supports basic features like pagination and search.
+SmartGrid JQuery Plugin
+========================
+
+Author: Filipe Rinaldi
+        filipe.rinaldi@gmail.com
+
+Introduction
+------------
+
+SmartGrid is a JQuery plugin that presents data in different views (list or tiles) and supports basic features like pagination and search. The plugin has the following features:
+
+- JSON based data input
+- Embedded search (_under development_)
+- Order by option
+- Multi-view (list|tiles)
+- Pagination (_under development_)
+- Add/remove data during run-time
+
+Demo
+----
+
+This [demo][1] page shows some examples of Smart Grid.
+
+Getting Started
+---------------
+
+    <div id="component"></div>
+    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="jquery.smartgrid.min.js"></script>
+    <script>
+    $('#component').smartGrid({
+        items : [{
+                    id : 0,
+                    name : John,
+                    surname : Doe,
+                    email: john.doe@email.com,
+                },{
+                    id : 1,
+                    name : Mary,
+                    surname : Doe,
+                    email: mary.doe@email.com,
+                }]
+    });
+    </script>
+
+Options
+-------
+
+| Name | Default | Description |
+|--------|---------|-------------|
+|currentPage|`1`| Set default page|
+|fnContent|`null`|Callback used to fill in the item|
+|items|`[]`|List of items where each item is a JSON structure |
+|itemsPerPage|`'all'`| Number of items per page. The parameter can be a number or `'all'`|
+|orderBy|`null`| When set to `null`, the Order elements using the field |
+|orderByFields|`[]`| Fields used to order the items. Format: {key: *'field name'*, label: *'Label'*}.<br>Example: {key:'name', label:'Name'}|
+|viewMode|`'list'`| One of the following supported view modes: `'list'`, `'tiles'`|
+||||
+
+Methods
+-------
+
+### addItem(item)
+Summary:
+> Add an item to the component and update the UI. If the component is currently configured to show the data ordered, then the new item will be inserted in order.
+
+Parameters:
+> **item** - Item object to be added to the item list.
+
+Return:
+> None.
+
+Example:
+
+    var sg = $('#component').data('smartGrid'); 
+    sg.addItem({
+        "name" : "Harold",
+		"surname" : "Giraffe",
+		"email" : "harold@email.co.uk",
+		"phone" : "01223 77777"
+	});
+
+### orderBy(key)
+Summary:
+> Set the elements in ascending order defined by the field 'key'. The UI is updated with the new ordering. The 'key' parameter must be a valid entry in the 'orderByFields' option.
+
+Parameters:
+> **key** - Field name used to order the elements.
+
+Return:
+> **true** - If key is a valid entry in the 'orderByFields' option.
+> **false** - If key is not a valid entry in the 'orderByFields' option.
+
+Example:
+
+    var sg = $('#component').data('smartGrid');
+    sg.orderBy('surname');
+
+### removeItem(item)
+Summary:
+> Remove one or more items that match the parameter "item".
+
+Parameters:
+> **item** - Dictionary containing key and value.
+
+Return:
+> list of items removed.
+
+Example:
+
+    var sg = $('#component').data('smartGrid'); 
+    sg.removeItem({
+        "key" : "name",
+		"value" : "Harold",
+	});
+
+### setViewMode(mode)
+Summary:
+> Set the view mode.
+
+Parameters:
+> **mode** - One of the valid view modes: 'list' or 'tiles'.
+
+Return:
+> **true** - Returns true if mode is valid.
+> **false** - Returns false if mode is invalid.
+
+Example:
+
+    var sg = $('#component').data('smartGrid'); 
+    sg.setViewMode('tiles');
+
+  [1]: http://filiperinaldi.github.io/smartgrid/demo
---
+++ jquery.smartGrid.js
@@ -0,0 +1,215 @@
+﻿/* Smart Grid 
+ * Author: Filipe Rinaldi
+ * Copyright 2014
+ */
+(function ($, undefined) {
+
+	var defaults = {
+	    items: [],
+	    orderBy: null,
+	    orderByFields: null,
+	    viewMode: 'list',
+	    currentPage: 0,
+	    itemsPerPage: 'all',
+	    fnContent: null,
+	};
+
+	var sg = $.smartGrid = {version: '0.1'};
+
+	$.fn.smartGrid = function(param) {
+		/* Initialisation */
+		var options = {};
+		$.extend(options, defaults, param);
+		var element = $($(this)[0]);
+		var sg = new SmartGrid(element, options);
+		element.data('smartGrid', sg);
+		sg.update();
+		return this;
+	};
+
+	function SmartGrid(element, options) {
+		
+		/* 
+		 * Attributes
+		 */
+		this.items = options.items;
+		delete options.items;
+		this.options = options;
+
+		/* 
+		 * Methods
+		 */
+		this.addItem = addItem;
+		this.orderBy = orderBy;
+		this.removeItem = removeItem;
+		this.search = search;
+		this.setViewMode = setViewMode;
+		this.sort = sort;
+		this.update = update;
+
+		if (this.options.orderBy)
+			this.sort();		
+
+		/* 
+		 * Create HTML
+		 */
+		element.addClass('sg-container thumbnail');
+
+		var order_options = "";
+		if (this.options.orderByFields) {
+			for (var i=0; i<this.options.orderByFields.length; i++) {
+				var option = this.options.orderByFields[i];
+				if (this.options.orderBy == option['key'])
+					var selected = ' selected';
+				else
+					var selected = '';
+				order_options += '<option value="'+option['key']+'" '+selected+'>'+option['label']+'</option>';
+			}
+		}
+
+		$('<div class="sg-header">\
+				<form class="form-inline">\
+					<input class="input-medium search-query" type="text" placeholder="Search">\
+					Order by:\
+					<select class="">\
+					'+order_options+'\
+					</select>\
+					<div class="btn-group pull-right">\
+			    		<a class="btn" href="#"><i class="icon-th-large"></i></a>\
+			    		<a class="btn" href="#"><i class="icon-align-justify"></i></a>\
+					</div>\
+				</form>\
+		   </div>\
+		   <hr>').prependTo(element);
+
+		/* Create content area */
+		this.contents = $('<ul class="thumbnails"></ul>').appendTo(element);
+
+		/* Create pagination */
+		$('<div class="pagination pagination-centered">\
+		   <ul>\
+		     <li><a href="#">&laquo;</a></li>\
+		     <li><a href="#">1</a></li>\
+		     <li><a href="#">2</a></li>\
+		     <li><a href="#">3</a></li>\
+		     <li><a href="#">&raquo;</a></li>\
+		   </ul>\
+		   </div>').appendTo(element);
+
+		/*
+		 * Bind controls
+		 */
+		this.btTiles = element.find('a').eq(0);
+		this.btTiles.data('smartGrid', this);
+		this.btTiles.click(function(){
+			var sg = $(this).data('smartGrid');
+			sg.setViewMode('tiles');
+		});
+		if (this.options.viewMode == 'tiles')
+			this.btTiles.addClass('disabled');
+
+		this.btList = element.find('a').eq(1);
+		this.btList.data('smartGrid', this);
+		this.btList.click(function(){
+			var sg = $(this).data('smartGrid');
+			sg.setViewMode('list');
+		});
+		if (this.options.viewMode == 'list')
+			this.btList.addClass('disabled');
+
+		this.selectOrder = element.find('select').eq(0);
+		this.selectOrder.data('smartGrid', this);
+		this.selectOrder.change(function(){
+			var sg = $(this).data('smartGrid');
+			var key = $(this).children(':selected').val();
+			sg.orderBy(key);
+		});
+
+		this.inputSearch = element.find('input').eq(0);
+		this.inputSearch.data('smartGrid', this);
+		this.inputSearch.keyup(function(){
+			var sg = $(this).data('smartGrid');
+			var text = $(this).val();
+			sg.search(text);
+		});
+
+		/*
+		 * Methods implementation
+		 */
+		function update() {
+			this.contents.empty();
+			for (var i=0; i<this.items.length; i++) {
+				var item = this.items[i]
+				$(this.options.fnContent(item, this.options.viewMode)).appendTo(this.contents);
+			}
+		};
+
+		function setViewMode(mode) {
+;;;			console.assert(mode == 'list' || mode == 'tiles', "Error: Invalid view mode");
+			this.options.viewMode = mode;
+			if (mode == 'list') {
+				this.btList.addClass('disabled');
+				this.btTiles.removeClass('disabled');	
+			} else if (mode == 'tiles') {
+				this.btTiles.addClass('disabled');
+				this.btList.removeClass('disabled');
+			} else {
+				return false;
+			}
+			this.update();
+			return true;
+		};
+
+		function search(text) {
+;;;			console.debug(text);
+			this.update();
+		}
+
+		function addItem(item) {
+			this.items.push(item);
+			if (this.options.orderBy)
+				this.sort();
+			this.update();	
+		}
+
+		function removeItem(item) {
+			var i = this.items.length;
+			var removed = [];
+			while (i--) {
+				var node = this.items[i];
+				if (node[item['key']] == item['value']) {
+					removed.push(node);
+					this.items.splice(i, 1);
+				}
+			}
+			if (removed.length != 0)
+				this.update();
+			return removed;
+		}
+
+		function orderBy(key) {
+			for (var i=0; i<this.options.orderByFields.length; i++) {
+				if (this.options.orderByFields[i].key === key) {
+					this.options.orderBy = key;
+					this.sort();
+					this.update();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		function sort() {
+			var key = this.options.orderBy;
+			this.items.sort(function(a, b){
+				//TODO: Add more cleverness to sort dates, numbers, case-insensitive, etc
+				if (a[key] < b[key])
+					return -1;
+				if (a[key] > b[key])
+					return 1;
+				if (a[key] == b[key])
+					return 0;
+			})
+		}
+	};
+})(jQuery);
